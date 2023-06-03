9c6ad8c34ea89a4385d776a953ce9c06860a2a77
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2023-06-03 20:47:41 +0200

    my first component

--- src/widgets/tables/monthly-cost-table.tsx
+++ src/widgets/tables/monthly-cost-table.tsx
@@ -1,16 +1,4 @@
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  IconButton,
-  Menu,
-  MenuHandler,
-  MenuItem,
-  MenuList,
-  Typography
-} from "@material-tailwind/react";
-import {CheckIcon, EllipsisVerticalIcon} from "@heroicons/react/24/outline";
+import {Card, CardBody, CardFooter, CardHeader, Chip, Typography} from "@material-tailwind/react";
 import {CostData, CostEntry} from "@/data";
 import React from "react";
 
@@ -39,36 +27,27 @@ export function MonthlyCostTable(props: TableProperties): JSX.Element {
         color="transparent"
         className="m-0 flex items-center justify-between p-6"
       >
-        <div>
+        <div className="flex items-center flex-row justify-start space-x-2">
           <Typography variant="h6" color="blue-gray" className="mb-1">
             Elementy opłat
           </Typography>
-          <Typography
-            variant="small"
-            className="flex items-center gap-1 font-normal text-blue-gray-600"
-          >
-            <CheckIcon strokeWidth={3} className="h-4 w-4 text-blue-500"/>
-            <strong>obecne</strong>
-          </Typography>
+          <Chip
+            size="sm"
+            variant="ghost"
+            color="green"
+            value="Aktualne"
+            icon={<span className="content-[''] block w-2 h-2 rounded-full mx-auto mt-1 bg-green-900"/>}
+          />
+          <Chip
+            size="sm"
+            variant="ghost"
+            color="red"
+            value="Brak Płatności"
+            icon={<span className="content-[''] block w-2 h-2 rounded-full mx-auto mt-1 bg-red-900"/>}
+          />
         </div>
-        <Menu placement="left-start">
-          <MenuHandler>
-            <IconButton size="sm" variant="text" color="blue-gray">
-              <EllipsisVerticalIcon
-                strokeWidth={3}
-                fill="currenColor"
-                className="h-6 w-6"
-              />
-            </IconButton>
-          </MenuHandler>
-          <MenuList>
-            <MenuItem>Action</MenuItem>
-            <MenuItem>Another Action</MenuItem>
-            <MenuItem>Something else here</MenuItem>
-          </MenuList>
-        </Menu>
       </CardHeader>
-      <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+      <CardBody className="px-0 pt-0 pb-2">
         <table className="w-full min-w-[640px] table-auto">
           <thead>
           <tr>
